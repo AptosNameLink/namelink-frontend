099639c0fc705ba78be0edd9af6333a1e2c0c5a0
Danna <danna.wa@kakaobrain.com>
2023-01-31 00:37:27 +0900

    design: fix border radius

--- src/components/ConnectWallet/ConnectWalletModal.tsx
+++ src/components/ConnectWallet/ConnectWalletModal.tsx
@@ -10,7 +10,7 @@ function ConnectWalletModal() {
     <div>
       <input type="checkbox" id="my-modal" className="modal-toggle" />
       <div className="modal">
-        <div className="modal-box min-w-fit border-[1px] border-namelink-gray-4 bg-namelink-gray-7 flex flex-col items-center p-[32px]">
+        <div className="modal-box min-w-fit border-[1px] border-namelink-gray-4 bg-namelink-gray-7 flex flex-col items-center p-[32px] rounded-[20px]">
           <h1 className="font-bold text-[48px] text-namelink-primary mt-[16px]">
             Sign in your Wallet
           </h1>
@@ -34,7 +34,7 @@ function ConnectWalletModal() {
               address={'0x2599 ...e6ac'}
             />
           </div>
-          <button className="btn mt-[39px] w-[698px] h-[78px] text-[26px] bg-gradient-to-b from-[#39CBA4] via-[#18DCAD] via-[#B2D2EF] to-[#D9D5C2] text-namelink-gray-8">
+          <button className="btn mt-[39px] w-[698px] h-[78px] text-[26px] bg-gradient-to-b from-[#39CBA4] via-[#18DCAD] via-[#B2D2EF] to-[#D9D5C2] text-namelink-gray-8 rounded-[16px]">
             MAPPING YOUR ADDRESS
           </button>
         </div>
