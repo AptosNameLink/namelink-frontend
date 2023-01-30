f8a891510945cbcb7b330a254010ef8c6e7a647d
Danna <danna.wa@kakaobrain.com>
2023-01-31 00:26:43 +0900

    chore: add tailwind custom color

--- tailwind.config.js
+++ tailwind.config.js
@@ -15,4 +15,22 @@ module.exports = {
     prefix: '',
     darkTheme: 'light',
   },
+
+  theme: {
+    colors: {
+      'namelink-primary': '#39CBA4',
+      'namelink-gray-0': '#FFFFFF',
+      'namelink-gray-1': '#CDD5E1',
+      'namelink-gray-2': '#9CA3B6',
+      'namelink-gray-3': '#5B647B',
+      'namelink-gray-4': '#292E3E',
+      'namelink-gray-5': '#1F232F',
+      'namelink-gray-6': '#151920',
+      'namelink-gray-7': '#101419',
+      'namelink-gray-8': '#0F1113',
+      'namelink-sub-1': '#B2D2EF',
+      'namelink-sub-2': '#D9D5C2',
+      'namelink-alert': '#BC4E4E',
+    },
+  },
 };
